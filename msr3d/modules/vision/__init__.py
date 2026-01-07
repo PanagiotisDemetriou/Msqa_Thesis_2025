38dd8277cb87a8669e6992bda2133e3c460a7aec
PanagiotisDemetriou <pdemet07@ucy.ac.cy>
2026-01-07 16:19:50 +0200

    Update __init__.py

--- msr3d/modules/vision/__init__.py
+++ msr3d/modules/vision/__init__.py
@@ -3,6 +3,7 @@ from .pcd_pointnet_encoder import *
 from .pcd_pointcept_encoder import *
 from .pcd_tokenize_encoder import *
 from .pcd_pointbert_encoder import *
+from .pcd_pointcept_encoder import *
 from .timm import Backbone2D, BLIP2Backbone
 
 # from .pcd_swin3d_encoder import *
