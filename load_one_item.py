7e96d0908e813f897246d78c7a25d0e1ba27e9b6
PanagiotisDemetriou <pdemet07@ucy.ac.cy>
2026-01-07 17:12:55 +0200

    Update load_one_item.py

--- load_one_item.py
+++ load_one_item.py
@@ -515,7 +515,7 @@ def main():
 
     # Build encoder once
     encoder = PTv3PcdObjEncoder(
-        cfg=args.cfg,
+        cfg=cfg,
         ptv3_cfg_path=args.ptv3_cfg,
         weight_path=args.ptv3_ckpt,
         sem_num_classes=args.sem_classes,
