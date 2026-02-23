7247e51f350efaddcb84b10af65b67eb3ac86c7a
PanagiotisDemetriou <pdemet07@ucy.ac.cy>
2026-02-23 22:20:00 +0200

    Update ptv3_data_processing.py

--- msr3d/data/datasets/ptv3_data_processing.py
+++ msr3d/data/datasets/ptv3_data_processing.py
@@ -58,7 +58,7 @@ def pool_features_scatter(obj_data):
 
 class PTV3DataProcessing():
    def __init__(self, cfg):
-      self.ptv3_cfg = ptv3_cfg = PCConfig.fromfile(cfg.model.prompter.model.vision.args.ptv3_cfg_path)
+      self.ptv3_cfg = ptv3_cfg = PCConfig.fromfile(cfg.args.ptv3_cfg_path)
       self.transform_cfg = ptv3_cfg.data.train.datasets[1]['transform']
       self.transform = Compose(self.transform_cfg)
 
