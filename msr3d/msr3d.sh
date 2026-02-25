58aa10dd0b00048ad74ade96715da8ffc342af47
PanagiotisDemetriou <pdemet07@ucy.ac.cy>
2026-02-25 10:42:42 +0200

    2 gpus

--- msr3d/msr3d.sh
+++ msr3d/msr3d.sh
@@ -1,1 +1,1 @@
-python launch.py --mode accelerate --name msr3d --qos lv1 --gpu_per_node 1 --num_nodes 1 --port 2341 --config configs/ptv3.yaml
+python launch.py --mode accelerate --name msr3d --qos lv1 --gpu_per_node 2 --num_nodes 1 --port 2341 --config configs/ptv3.yaml
